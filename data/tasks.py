b1e60c26790b4850b3c45db8d212d6240563dcdb
etyenneprezident-ui <etyenneprezident@gmail.com>
2026-01-22 19:53:02 +0300

    sql

---
+++ data/tasks.py
@@ -0,0 +1,22 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Tasks(SqlAlchemyBase):
+    __tablename__ = 'tasks'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    statement = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    input_format = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    output_format = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    memory_limit = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    difficulty = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    task_tests = orm.relationship("TaskTest", back_populates='tasks')
+    submissions = orm.relationship("Submissions", back_populates="tasks")
